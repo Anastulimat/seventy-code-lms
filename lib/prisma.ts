63ac5bbc6b0f4d1d5d84824ce70fcce59e940426
Anas TULIMAT <anas.tulimat@orange.com>
2025-10-28 16:50:16 +0100

    feat: add arcjet integration

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,4 @@
-// lib/prisma.ts
-
-import { PrismaClient } from "./generated/prisma";
+import {PrismaClient} from "./generated/prisma";
 
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
