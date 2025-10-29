7014df1e065c055fc66d3d94418e10ee169d5995
Anas TULIMAT <anas.tulimat@orange.com>
2025-10-29 23:43:54 +0100

    chore: clean up code

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -5,24 +5,20 @@ import {
     IconCamera,
     IconChartBar,
     IconDashboard,
-    IconDatabase,
     IconFileAi,
     IconFileDescription,
-    IconFileWord,
     IconFolder,
     IconHelp,
     IconInnerShadowTop,
     IconListDetails,
-    IconReport,
     IconSearch,
     IconSettings,
     IconUsers,
 } from "@tabler/icons-react"
 
-import {NavDocuments} from "@/components/nav-documents"
-import {NavMain} from "@/components/nav-main"
-import {NavSecondary} from "@/components/nav-secondary"
-import {NavUser} from "@/components/nav-user"
+import {NavMain} from "@/components/sidebar/nav-main"
+import {NavSecondary} from "@/components/sidebar/nav-secondary"
+import {NavUser} from "@/components/sidebar/nav-user"
 import {
     Sidebar,
     SidebarContent,
@@ -33,12 +29,9 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar"
 
+// ----------------------------------------------------------------------
+
 const data = {
-    user: {
-        name: "shadcn",
-        email: "m@example.com",
-        avatar: "/avatars/shadcn.jpg",
-    },
     navMain: [
         {
             title: "Dashboard",
@@ -131,24 +124,9 @@ const data = {
             icon: IconSearch,
         },
     ],
-    documents: [
-        {
-            name: "Data Library",
-            url: "#",
-            icon: IconDatabase,
-        },
-        {
-            name: "Reports",
-            url: "#",
-            icon: IconReport,
-        },
-        {
-            name: "Word Assistant",
-            url: "#",
-            icon: IconFileWord,
-        },
-    ],
 }
+
+// ----------------------------------------------------------------------
 
 export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
     return (
@@ -162,7 +140,7 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
                         >
                             <a href="#">
                                 <IconInnerShadowTop className="!size-5"/>
-                                <span className="text-base font-semibold">Acme Inc.</span>
+                                <span className="text-base font-semibold">SeventyCode LMS.</span>
                             </a>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
@@ -170,11 +148,10 @@ export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
             </SidebarHeader>
             <SidebarContent>
                 <NavMain items={data.navMain}/>
-                <NavDocuments items={data.documents}/>
                 <NavSecondary items={data.navSecondary} className="mt-auto"/>
             </SidebarContent>
             <SidebarFooter>
-                <NavUser user={data.user}/>
+                <NavUser/>
             </SidebarFooter>
         </Sidebar>
     )
