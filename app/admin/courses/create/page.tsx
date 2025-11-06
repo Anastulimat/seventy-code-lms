19b09a7d12dd04f42efe15de4f4654dd21579214
Anas TULIMAT <anas.tulimat@orange.com>
2025-11-06 16:14:27 +0100

    feat!: add canva confetti package and use confetti hook

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -19,12 +19,14 @@ import {tryCatch} from "@/hooks/try-catch";
 import {createCourse} from "@/app/admin/courses/create/actions";
 import {toast} from "sonner";
 import {useRouter} from "next/navigation";
+import {useConfetti} from "@/hooks/use-confetti";
 
 // ----------------------------------------------------------------------
 
 export default function CreateCoursePage() {
     const [isPending, startTransition] = useTransition();
     const router = useRouter();
+    const {triggerConfetti} = useConfetti();
 
     const form = useForm<CourseSchemaType>({
         resolver: zodResolver(courseSchema) as never,
@@ -53,6 +55,7 @@ export default function CreateCoursePage() {
 
             if (result?.status === 'success') {
                 toast.success(result.message);
+                triggerConfetti();
                 form.reset();
                 router.push('/admin/courses');
             } else if (result?.status === 'error') {
